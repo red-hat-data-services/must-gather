90e3f61363889851943a32c4afc45b136495fb72
Christoph Stäbler <cstabler@redhat.com>
2026-06-29 11:16:21 +0200

    Use full API group

--- collection-scripts/gather_mcp_lifecycle_operator.sh
+++ collection-scripts/gather_mcp_lifecycle_operator.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC1091
 : "${SCRIPT_DIR:=$(dirname "$0")}"
 source "${SCRIPT_DIR}/common.sh"
-resources=("mcpservers")
+resources=("mcpservers.mcp.x-k8s.io")
 
 nslist=$(get_all_namespace "${resources[@]}")
 
